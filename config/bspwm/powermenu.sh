ea81b10686f1b4659ad21c538380829d004058ed
PurpleUltraViolet <purple.ultraviolet616@gmail.com>
2021-01-22 14:48:22 -0700

    Remove hibernate from power menu - never use it

--- config/bspwm/powermenu.sh
+++ config/bspwm/powermenu.sh
@@ -7,14 +7,13 @@ uptime=$(uptime -p | sed 's/up //g')
 # Options
 lock="lock"
 suspend="suspend"
-hibernate="hibernate"
 shutdown="shutdown"
 reboot="reboot"
 logout="quit"
 dir="$HOME/.config/rofi/power"
 
 # Variable passed to rofi
-options="$lock\n$suspend\n$hibernate\n$shutdown\n$reboot\n$logout"
+options="$lock\n$suspend\n$shutdown\n$reboot\n$logout"
 optnum=$(echo -e "$options" | wc -l)
 
 chosen="$(echo -e "$options" | $rofi_command -lines $optnum -i\
@@ -51,18 +50,6 @@ case $chosen in
         if [[ $ans == "yes" ]] || [[ $ans == "y" ]]; then
         mpc -q pause >&2 /dev/null
         systemctl suspend
-        elif [[ $ans == "no" ]] || [[ $ans == "n" ]]; then
-        exit
-        else
-        rofi -e "Available Options  -  yes / y / no / n"
-        fi
-        ;;
-    $hibernate)
-        ans=$(eval "$confirm_command")
-        ans=$(echo $ans | awk '{print tolower($0)}')
-        if [[ $ans == "yes" ]] || [[ $ans == "y" ]]; then
-        mpc -q pause >&2 /dev/null
-        systemctl hibernate
         elif [[ $ans == "no" ]] || [[ $ans == "n" ]]; then
         exit
         else
