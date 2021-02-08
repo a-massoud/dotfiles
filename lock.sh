ebce7a4540a717f0f2d73030d3de5669ca52c7ff
PurpleUltraViolet <purple.ultraviolet616@gmail.com>
2021-02-08 14:45:10 -0700

    Move to Dracula theme
    removing neomutt stuff

--- lock.sh
+++ lock.sh
@@ -4,17 +4,22 @@ pkill -SIGUSR1 dunst
 
 i3lock -nk \
        --pass-screen-keys \
-       --color=383c4a \
-       --insidevercolor=383c4a \
-       --insidewrongcolor=383c4a \
-       --insidecolor=383c4a \
-       --ringcolor=2c2f3a \
-       --ringvercolor=2c2f3a \
-       --ringwrongcolor=2c2f3a \
+       --color=282a36 \
+       --insidevercolor=282a36 \
+       --insidewrongcolor=282a36 \
+       --insidecolor=282a36 \
+       --ringcolor=44475a \
+       --ringvercolor=44475a \
+       --ringwrongcolor=44475a \
        --linecolor=00000000 \
-       --keyhlcolor=d3dae3 \
-       --bshlcolor=d3dae3 \
+       --keyhlcolor=6272a4 \
+       --bshlcolor=6272a4 \
        --separatorcolor=00000000 \
+       --timecolor=f8f8f2 \
+       --datecolor=f8f8f2 \
+       --verifcolor=f8f8f2 \
+       --wrongcolor=f8f8f2 \
+       --layoutcolor=f8f8f2 \
        --time-font=NotoSans-Regular \
        --timesize=24 \
        --date-font=NotoSans-Regular \
