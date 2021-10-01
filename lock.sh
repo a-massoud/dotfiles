675df9d3272082f72f742290eea42ed416f9c06d
PurpleUltraViolet <purple.ultraviolet616@gmail.com>
2021-09-30 17:51:36 -0700

    Update i3lock-color

--- lock.sh
+++ lock.sh
@@ -5,34 +5,34 @@ pkill -SIGUSR1 dunst
 i3lock -nk \
        --pass-screen-keys \
        --color=282a36 \
-       --insidevercolor=282a36 \
-       --insidewrongcolor=282a36 \
-       --insidecolor=282a36 \
-       --ringcolor=44475a \
-       --ringvercolor=44475a \
-       --ringwrongcolor=44475a \
-       --linecolor=00000000 \
-       --keyhlcolor=6272a4 \
-       --bshlcolor=6272a4 \
-       --separatorcolor=00000000 \
-       --timecolor=f8f8f2 \
-       --datecolor=f8f8f2 \
-       --verifcolor=f8f8f2 \
-       --wrongcolor=f8f8f2 \
-       --layoutcolor=f8f8f2 \
+       --insidever-color=282a36 \
+       --insidewrong-color=282a36 \
+       --inside-color=282a36 \
+       --ring-color=44475a \
+       --ringver-color=44475a \
+       --ringwrong-color=44475a \
+       --line-color=00000000 \
+       --keyhl-color=6272a4 \
+       --bshl-color=6272a4 \
+       --separator-color=00000000 \
+       --time-color=f8f8f2 \
+       --date-color=f8f8f2 \
+       --verif-color=f8f8f2 \
+       --wrong-color=f8f8f2 \
+       --layout-color=f8f8f2 \
        --time-font=NotoSans-Regular \
-       --timesize=24 \
+       --time-size=24 \
        --date-font=NotoSans-Regular \
-       --datesize=24 \
+       --date-size=24 \
        --layout-font=NotoSans-Regular \
-       --layoutsize=24 \
+       --layout-size=24 \
        --verif-font=NotoSans-Regular \
-       --verifsize=24 \
+       --verif-size=24 \
        --wrong-font=NotoSans-Regular \
-       --wrongsize=24 \
-       --timestr="%T" \
-       --datestr="%Y-%m-%0d" \
-       --wrongtext="wrong" \
+       --wrong-size=24 \
+       --time-str="%T" \
+       --date-str="%Y-%m-%0d" \
+       --wrong-text="wrong" \
        --radius=100
 
 pkill -SIGUSR2 dunst
