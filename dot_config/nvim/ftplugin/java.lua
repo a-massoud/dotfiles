bd62ab170b51b2b1ce5e727cc169e23b61ca50aa
a-massoud <purple.ultraviolet616@gmail.com>
2023-08-07 23:34:02 -0700

    Move back to vim full-time

---
+++ dot_config/nvim/ftplugin/java.lua
@@ -0,0 +1,24 @@
+-- jdtls
+local config = {
+    cmd = { '/usr/bin/jdtls' },
+    root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw', 'pom.xml' }, { upward = true })[1]),
+    runtimes = {
+        {
+            name = 'JavaSE-1.8',
+            path = '/usr/lib/jvm/java-8-openjdk',
+            javadoc = 'https://docs.oracle.com/en/java/javase/8/docs/api'
+        },
+        {
+            name = 'JavaSE-11',
+            path = '/usr/lib/jvm/java-11-openjdk',
+            javadoc = 'https://docs.oracle.com/en/java/javase/11/docs/api'
+        },
+        {
+            name = 'JavaSE-17',
+            path = '/usr/lib/jvm/java-17-openjdk',
+            javadoc = 'https://docs.oracle.com/en/java/javase/17/docs/api',
+            default = true
+        }
+    }
+}
+require('jdtls').start_or_attach(config)
--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -43,6 +43,28 @@ vim.fn['plug#']('dhruvasagar/vim-table-mode')
 
 -- utl
 vim.fn['plug#']('vim-scripts/utl.vim')
+vim.fn['plug#']('honza/vim-snippets')
+
+-- snippets
+vim.fn['plug#']('SirVer/ultisnips')
+
+-- lsp
+vim.fn['plug#']('neovim/nvim-lspconfig')
+vim.fn['plug#']('hrsh7th/cmp-nvim-lsp')
+vim.fn['plug#']('hrsh7th/cmp-buffer')
+vim.fn['plug#']('hrsh7th/cmp-path')
+vim.fn['plug#']('hrsh7th/cmp-cmdline')
+vim.fn['plug#']('hrsh7th/nvim-cmp')
+vim.fn['plug#']('quangnguyen30192/cmp-nvim-ultisnips')
+
+-- jdtls
+vim.fn['plug#']('mfussenegger/nvim-jdtls')
+
+-- tex
+vim.fn['plug#']('lervag/vimtex')
+
+-- emmet
+vim.fn['plug#']('mattn/emmet-vim')
 
 vim.call('plug#end')
 
@@ -68,6 +90,8 @@ vim.opt.title = true
 vim.opt.titlelen = 0
 vim.opt.fixeol = false
 vim.opt.delcombine = true
+vim.opt.spell = false
+vim.opt.spelllang = 'en_us,es_mx,fr,cjk'
 
 -- search options
 vim.opt.hlsearch = true
@@ -130,14 +154,14 @@ vim.api.nvim_create_autocmd({ 'TermOpen' }, {
 })
 
 -- telescope
-require("telescope").setup {
+require('telescope').setup {
     extensions = {
         file_browser = {
             hijack_netrw = true
         }
     }
 }
-require("telescope").load_extension "file_browser"
+require('telescope').load_extension 'file_browser'
 
 vim.api.nvim_set_keymap(
     'n',
@@ -209,3 +233,173 @@ vim.api.nvim_set_keymap(
     '<Esc><cmd>set fo+=t<CR>a',
     { noremap = true }
 )
+
+-- ultisnips
+vim.g.UltiSnipsExpandTrigger = '<tab>'
+vim.g.UltiSnipsJumpForwardTrigger = '<c-w>'
+vim.g.UltiSnipsJumpBackwardTrigger = '<c-q>'
+
+-- nvim-cmp.
+local cmp = require 'cmp'
+
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            vim.fn['UltiSnips#Anon'](args.body) -- For `ultisnips` users.
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'ultisnips' }, -- For ultisnips users.
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+        { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+})
+
+-- lspconfig
+vim.opt.signcolumn = 'yes'
+-- Setup language servers.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup { capabilities = capabilities }
+lspconfig.tsserver.setup { capabilities = capabilities }
+lspconfig.rust_analyzer.setup {
+    settings = {
+        ['rust-analyzer'] = {
+            imports = {
+                granularity = {
+                    group = 'module'
+                },
+            },
+        },
+    },
+    capabilities = capabilities
+}
+lspconfig.clangd.setup { capabilities = capabilities }
+lspconfig.cmake.setup { capabilities = capabilities }
+lspconfig.lua_ls.setup {
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file('', true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+    capabilities = capabilities
+}
+lspconfig.texlab.setup { capabilities = capabilities }
+lspconfig.html.setup { capabilities = capabilities }
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        -- disable virtual text
+        virtual_text = true,
+
+        -- show signs
+        signs = true,
+
+        -- delay update diagnostics
+        update_in_insert = true,
+        -- display_diagnostic_autocmds = { "InsertLeave" },
+
+    }
+)
+
+-- vimtex
+vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_compiler_latexmk = {
+    out_dir = './out'
+}
