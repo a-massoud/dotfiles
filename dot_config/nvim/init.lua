2c22abc44fbc9359faca4b9cb7c8a62c17cfec0c
a-massoud <purple.ultraviolet616@gmail.com>
2023-08-12 00:18:08 -0700

    Give tree-sitter another shot

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -56,6 +56,9 @@ vim.fn['plug#']('hrsh7th/cmp-cmdline')
 vim.fn['plug#']('hrsh7th/nvim-cmp')
 vim.fn['plug#']('quangnguyen30192/cmp-nvim-ultisnips')
 
+-- tree-sitter
+vim.fn['plug#']('nvim-treesitter/nvim-treesitter', { ['do'] = 'TSUpdate' })
+
 -- jdtls
 vim.fn['plug#']('mfussenegger/nvim-jdtls')
 
@@ -434,6 +437,22 @@ else
 end
 
 
+-- tree-sitter
+require 'nvim-treesitter.configs'.setup {
+    sync_install = true,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true
+    }
+}
 
 -- vimtex
 vim.g.vimtex_view_method = 'zathura'
