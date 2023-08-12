358fc78829ad3476e1f3828f1abd91fd7c2c29e2
a-massoud <purple.ultraviolet616@gmail.com>
2023-08-11 23:00:24 -0700

    Move to Fira font

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -364,9 +364,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         local opts = { buffer = ev.buf }
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
         vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
         vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
         vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
@@ -376,12 +376,38 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
+        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>fmt', function()
             vim.lsp.buf.format { async = true }
         end, opts)
     end,
 })
+
+vim.api.nvim_set_keymap(
+    'n',
+    '<space>gr',
+    '<cmd>Telescope lsp_references<cr>',
+    { noremap = true }
+)
+vim.api.nvim_set_keymap(
+    'n',
+    '<space>gd',
+    '<cmd>Telescope lsp_definitions<cr>',
+    { noremap = true }
+)
+vim.api.nvim_set_keymap(
+    'n',
+    '<space>gi',
+    '<cmd>Telescope lsp_implementations<cr>',
+    { noremap = true }
+)
+vim.api.nvim_set_keymap(
+    'n',
+    '<space>fs',
+    '<cmd>Telescope lsp_workspace_symbols<cr>',
+    { noremap = true }
+)
+
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
