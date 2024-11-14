e699d8acd35a6da1af2e2633989cc2fdd97fffcc
a-massoud <purple.ultraviolet616@gmail.com>
2024-11-13 18:29:46 -0700

    Add noice.nvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -57,7 +57,8 @@ require("lazy").setup({
 	"R-nvim/cmp-r",
 	"hkupty/iron.nvim",
 	"stevearc/dressing.nvim",
-	"rcarriga/nvim-notify",
+	{ "rcarriga/nvim-notify", lazy = false },
+	{ "folke/noice.nvim", lazy = false },
 	"stevearc/overseer.nvim",
 	"Civitasv/cmake-tools.nvim",
 })
@@ -376,6 +377,9 @@ lspconfig.ltex.setup({
 				fr = { "Massoud" },
 			},
 			disabledRules = { fr = { "FRENCH_WHITESPACE" } },
+			["ltex-ls"] = {
+				logLevel = { "severe" },
+			},
 		},
 	},
 	on_attach = function(client, bufnr)
@@ -622,7 +626,40 @@ vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
 vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
 vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
 
+-- notify & noice
 vim.notify = require("notify")
+require("noice").setup({
+	lsp = {
+		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+		override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+		},
+	},
+	-- you can enable a preset for easier configuration
+	presets = {
+		bottom_search = true, -- use a classic bottom cmdline for search
+		command_palette = true, -- position the cmdline and popupmenu together
+		long_message_to_split = true, -- long messages will be sent to a split
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		lsp_doc_border = false, -- add a border to hover docs and signature help
+	},
+    -- Disable ltex spam
+	routes = {
+		{
+			filter = {
+				event = "lsp",
+				kind = "progress",
+				cond = function(message)
+					local client = vim.tbl_get(message.opts, "progress", "client")
+					return client == "ltex"
+				end,
+			},
+			opts = { skip = true },
+		},
+	},
+})
 
 -- overseer
 require("overseer").setup()
