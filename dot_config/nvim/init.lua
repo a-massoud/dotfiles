2365372e318b281fea20de1300b4c240b70a4585
a-massoud <purple.ultraviolet616@gmail.com>
2024-11-21 09:23:29 -0700

    Pretty is distracting

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -55,9 +55,9 @@ require("lazy").setup({
 	{ "R-nvim/R.nvim", lazy = false },
 	"R-nvim/cmp-r",
 	"hkupty/iron.nvim",
-	"stevearc/dressing.nvim",
-	{ "rcarriga/nvim-notify", lazy = false },
-	{ "folke/noice.nvim", lazy = false },
+	-- "stevearc/dressing.nvim",
+	-- { "rcarriga/nvim-notify", lazy = false },
+	-- { "folke/noice.nvim", lazy = false },
 	"stevearc/overseer.nvim",
 })
 
@@ -162,7 +162,7 @@ lualine.setup({
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = { "filename" },
-		lualine_x = { "overseer", "encoding", "fileformat", "filetype" },
+		lualine_x = { "overseer", "encoding", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
@@ -618,39 +618,39 @@ vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
 vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
 
 -- notify & noice
-vim.notify = require("notify")
-require("noice").setup({
-	lsp = {
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-		},
-	},
-	-- you can enable a preset for easier configuration
-	presets = {
-		bottom_search = true, -- use a classic bottom cmdline for search
-		command_palette = true, -- position the cmdline and popupmenu together
-		long_message_to_split = true, -- long messages will be sent to a split
-		inc_rename = false, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = false, -- add a border to hover docs and signature help
-	},
-	-- Disable ltex spam
-	routes = {
-		{
-			filter = {
-				event = "lsp",
-				kind = "progress",
-				cond = function(message)
-					local client = vim.tbl_get(message.opts, "progress", "client")
-					return client == "ltex"
-				end,
-			},
-			opts = { skip = true },
-		},
-	},
-})
+-- vim.notify = require("notify")
+-- require("noice").setup({
+-- 	lsp = {
+-- 		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+-- 		override = {
+-- 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+-- 			["vim.lsp.util.stylize_markdown"] = true,
+-- 			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+-- 		},
+-- 	},
+-- 	-- you can enable a preset for easier configuration
+-- 	presets = {
+-- 		bottom_search = true, -- use a classic bottom cmdline for search
+-- 		command_palette = true, -- position the cmdline and popupmenu together
+-- 		long_message_to_split = true, -- long messages will be sent to a split
+-- 		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+-- 		lsp_doc_border = false, -- add a border to hover docs and signature help
+-- 	},
+-- 	-- Disable ltex spam
+-- 	routes = {
+-- 		{
+-- 			filter = {
+-- 				event = "lsp",
+-- 				kind = "progress",
+-- 				cond = function(message)
+-- 					local client = vim.tbl_get(message.opts, "progress", "client")
+-- 					return client == "ltex"
+-- 				end,
+-- 			},
+-- 			opts = { skip = true },
+-- 		},
+-- 	},
+-- })
 
 -- overseer
 require("overseer").setup()
