a0d7934e8afb3fdabfc91d2ed54aa93b8ee6abc9
a-massoud <jacob.massoud04@gmail.com>
2026-01-17 09:32:49 -0700

    Remove languagetool. It's a resource hog

--- dot_config/nvim/lua/plugins/tex.lua
+++ dot_config/nvim/lua/plugins/tex.lua
@@ -1,38 +1,5 @@
 return {
   {
-    "mason-org/mason.nvim",
-    opts = { ensure_installed = { "ltex-ls-plus" } },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ltex_plus = {
-          settings = {
-            ltex = {
-              checkFrequency = "save",
-              language = "en-US",
-              additionalRules = {
-                enablePickyRules = true,
-                motherTongue = "en",
-              },
-              disabledRules = { fr = { "FRENCH_WHITESPACE" } },
-              languageToolHttpServerUri = "http://localhost:8081/",
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "barreiroleo/ltex_extra.nvim",
-    branch = "dev",
-    dependencies = { "neovim/nvim-lspconfig" },
-    opts = {
-      load_langs = { "en-US", "fr", "es", "ar" },
-    },
-  },
-  {
     "lervag/vimtex",
     init = function()
       vim.g.vimtex_view_general_viewer = "zathura"
