d5d3253cc1e1233e45072112ab6cb475df4dd480
a-massoud <jacob.massoud04@gmail.com>
2026-03-06 17:28:59 -0700

    Properly disable syntax

--- dot_config/nvim/lua/plugins/tex.lua
+++ dot_config/nvim/lua/plugins/tex.lua
@@ -4,22 +4,7 @@ return {
     init = function()
       vim.g.vimtex_view_general_viewer = "zathura"
       vim.g.vimtex_view_method = "zathura"
-      vim.g.vimtex_syntax_conceal = {
-        accents = 0,
-        ligatures = 0,
-        cites = 0,
-        fancy = 0,
-        texTabularChar = 0,
-        spacing = 0,
-        greek = 0,
-        math_bounds = 0,
-        math_delimiters = 0,
-        math_fracs = 0,
-        math_super_sub = 0,
-        math_symbols = 0,
-        sections = 0,
-        styles = 0,
-      }
+      vim.g.vimtex_syntax_conceal_disable = 1
     end,
   },
   {
